76f0205064fc8780d12064dfa814ea51c73d6ac9
Luka Jeremic <ljeremic4019rn@raf.rs>
2023-03-24 11:50:12 +0100

    napravljen domain sum worker

--- src/main/java/result/ResultRetriever.java
+++ src/main/java/result/ResultRetriever.java
@@ -6,10 +6,22 @@ import result.results.DirScanResult;
 import result.results.Result;
 import result.results.WebScanResult;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.*;
 
 public class ResultRetriever extends Thread {
+
+    private final List<String> allDomains = new ArrayList<>();
+    private final ExecutorCompletionService<Map<String, Integer>> completionService;
+//    private
+
+    public ResultRetriever() {
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        this.completionService = new ExecutorCompletionService<>(threadPool);
+    }
 
     @Override
     public void run() {
@@ -37,7 +49,7 @@ public class ResultRetriever extends Thread {
 
     //todo ulepsaj sve getove (da budu dinamicki)
 
-    public void getWebResult(String url){
+    public void getSingleUrlResult(String url){
         Map<String, Integer> scannerResult = null;
         WebScanResult webScanResult = App.webScannerResults.get(url);
 
@@ -51,15 +63,46 @@ public class ResultRetriever extends Thread {
         System.out.println(url + " = " + scannerResult);
     }
 
-    public void getWebSummary(){
-        Map<String, WebScanResult> resultsByDirectory = new HashMap<>();
+    public void getDomainResults(String domainUrl){
+        getAllDomains();
+        if (!allDomains.contains(domainUrl)) System.err.println("Domain with entered url not found in results- " + domainUrl);
 
-        for (Map.Entry<String, WebScanResult> result: App.webScannerResults.entrySet()) {
-            System.out.println(result.getKey() + " = " + result.getValue().getResult());
+        App.webDomainResults.put(domainUrl, this.completionService.submit(new WebDomainSumWorker(domainUrl)));
+
+        try {
+            Thread.sleep(2000);
+
+            System.out.println("Rezultat za domen: " + domainUrl);
+            System.out.println(App.webDomainResults.get(domainUrl).get());
+
+        }
+        catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
         }
     }
 
-    //todo sumiraj mape
+
+
+    public void getWebSummary(){
+        getAllDomains();
+
+        for (String dUrl :allDomains)
+            App.webDomainResults.put(dUrl, this.completionService.submit(new WebDomainSumWorker(dUrl)));
+
+        System.out.println(">> submitted " + allDomains.size() + " domains to calculate results");
+
+
+        try {
+            Thread.sleep(2000);
+
+            for (Map.Entry<String, Future<Map<String, Integer>>> domainRes: App.webDomainResults.entrySet()) {
+                System.out.println("Rezultat za: " + domainRes.getKey() + " = " + domainRes.getValue().get());
+            }
+
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void getFileResult(String corpusDirName){
         Map<String, Integer> scannerResult = null;
@@ -99,5 +142,14 @@ public class ResultRetriever extends Thread {
             else System.out.println(result.getKey() + " = " + result.getValue().getQueryResult());
         }
     }
+
+    private void getAllDomains(){
+        allDomains.clear();
+        for (Map.Entry<String, WebScanResult> result: App.webScannerResults.entrySet()) {
+            if (result.getValue().getResult().isEmpty() || result.getValue().getResult() == null) continue;
+            if (!allDomains.contains(result.getValue().getDomain())) allDomains.add(result.getValue().getDomain());
+        }
+    }
+
 
 }
---
+++ src/main/java/result/WebDomainSumWorker.java
@@ -0,0 +1,36 @@
+package result;
+
+import app.App;
+import result.results.WebScanResult;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class WebDomainSumWorker implements Callable<Map<String, Integer>> {
+
+    String domainUrl;
+
+    public WebDomainSumWorker(String domainUrl) {
+        this.domainUrl = domainUrl;
+    }
+
+    @Override
+    public Map<String, Integer> call() {
+        Map<String, Integer> domainResultMap = new ConcurrentHashMap<>();
+
+        //prodjemo kroz sve rezultate i trazimo domen koji nam treba
+        for (Map.Entry<String, WebScanResult> singleWebResult : App.webScannerResults.entrySet()) {
+
+            if (!singleWebResult.getValue().getDomain().equals(domainUrl)) continue;//ako nije onaj koji zelimo da sumiramo preskoci ga
+
+            for (Map.Entry<String, Integer> singlePageResults : singleWebResult.getValue().getResult().entrySet()) {
+                int oldValue = domainResultMap.getOrDefault(singlePageResults.getKey(), 0);
+                domainResultMap.put(singlePageResults.getKey(), oldValue + singlePageResults.getValue());//sumiramo sve vrednosti istih key word-ova za zeljeni domen
+            }
+        }
+        System.err.println("Zavrseno racunanje za domen " + domainUrl);
+        return domainResultMap;
+    }
+
+}
